fcaa05f4b796beb3bd9bfd00d69facbfe84bb643
giovanirubini <giovanirubinimaccagno@gmail.com>
2020-09-22 21:20:14 -0400

    display in front end screen

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -4,6 +4,17 @@ const User = require('../models/user');
 exports.getPosts = (req, res, next) => {
   Post.find()
     .then((posts) => {
-      console.log(posts);
+      if (!posts) {
+        const error = new Error('Posts could not be fetched');
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200).json({ message: 'Tweets retrieved succesfully', posts });
+    })
+    .catch(error => {
+      if (!error.statusCode) {
+        error.statusCode = 500;
+      }
+      next(error);
     });
 };
